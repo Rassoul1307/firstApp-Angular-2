2ec69dc3e6b27d0bdf5521477af71b48d9c34ebd
rassoul fall <fallrassoul342@gmail.com>
2024-01-05 03:13:52 +0000

    Mise à jour pour le déploiement sur GitHub Pages

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
+import { Title } from '@angular/platform-browser';
+
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
+
+  housingService: HousingService = inject(HousingService);
+  constructor() {
+    // Injectez le service Title
+    const titleService = inject(Title);
+    titleService.setTitle('Accueil');
+
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
